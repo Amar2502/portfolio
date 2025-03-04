a46f44f751a0a49d06b9ff14d9fb9a8f4c2df3d9
Amar Pandey <amarpandey2502@gmail.com>
2025-03-04 11:31:58 +0530

    added themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@
 import { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { ThemeProvider } from '@/components/theme-provider'
+import { ThemeProvider } from 'next-themes'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -59,10 +59,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <ThemeProvider
-          // attribute="class"
+          attribute="class"
           defaultTheme="system"
-          // enableSystem
-          // disableTransitionOnChange
+          enableSystem
+          disableTransitionOnChange={false}
         >
           {children}
         </ThemeProvider>
